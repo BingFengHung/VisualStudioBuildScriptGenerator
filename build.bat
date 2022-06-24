833ba7420e351adb0b9475a8d60a4a52ed22b586
BingFengHung <joe94008@gmail.com>
2022-06-24 13:45:03 +0800

    <chore>: 新增 build.bat

---
+++ build.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" "VisualStudioBuildScriptGenerator.sln" /t:Clean;Rebuild /p:Configuration=Debug
+
+cmd /k
