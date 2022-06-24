d1ffa4eca6b03a30bd0bcd3bba6bd12148d3f230
BingFengHung <joe94008@gmail.com>
2022-06-24 14:20:06 +0800

    <feat>: 加入 RelayCommand

---
+++ src/VisualStudioBuildScriptGenerator/Commands/RelayCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace VisualStudioBuildScriptGenerator
+{
+    class RelayCommand : ICommand
+    {
+        Action<object> _execute;
+        Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute) : this(execute, (i) => true)
+        {
+        }
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => _canExecute(parameter);
+
+        public void Execute(object parameter) => _execute(parameter);
+    }
+}
