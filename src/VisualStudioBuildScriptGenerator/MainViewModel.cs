94fca4320f516119eccc7044e9698567f1932b3e
BingFengHung <joe94008@gmail.com>
2022-06-24 16:23:36 +0800

    <feat>: 加入建立腳本程式

--- src/VisualStudioBuildScriptGenerator/MainViewModel.cs
+++ src/VisualStudioBuildScriptGenerator/MainViewModel.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
+﻿using System.Collections.ObjectModel;
+using System.IO;
 using System.Text;
+using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Input;
 
 namespace VisualStudioBuildScriptGenerator
@@ -25,6 +25,13 @@ namespace VisualStudioBuildScriptGenerator
 
             ConfigurationSelected = Configurations[0];
             PlatformSelected = Platforms[0];
+
+            FilesCopyPath = new ObservableCollection<FromToPathModel>
+            {
+                new FromToPathModel{SourceFilePath = "A", DestinationFolderPath = "B"},
+                new FromToPathModel{SourceFilePath = "C", DestinationFolderPath = "B"},
+                new FromToPathModel{SourceFilePath = "D", DestinationFolderPath = "B"}
+            };
         }
 
         public ObservableCollection<OptionModel> Configurations { get; set; }
@@ -35,12 +42,66 @@ namespace VisualStudioBuildScriptGenerator
 
         public OptionModel PlatformSelected { get; set; }
 
-        public ObservableCollection<string> FilesCopyPath { get; set; } = new ObservableCollection<string>();
+        public ObservableCollection<FromToPathModel> FilesCopyPath { get; set; } = new ObservableCollection<FromToPathModel>();
 
         public string ScriptEdit { get; set; }
 
         public ICommand AddCopyPathCommand => new RelayCommand(AddCopyPathCommandExecute);
+        public ICommand ScriptPreviewCommand => new RelayCommand(ScriptPreviewCommandExecute);
+        public ICommand OutputCommand => new RelayCommand(OutputCommandExecute);
 
+        public ICommand GetSlnNameCommand => new RelayCommand(GetSlnNameCommandExecute);
+
+        public string SlnName { get; set; }
+
+        private void GetSlnNameCommandExecute(object parameter)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                DialogResult result = dialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    SlnName = dialog.SafeFileName;
+                }
+            }
+        }
+
+        private string CreateScript()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var file in FilesCopyPath)
+            {
+                sb.AppendLine($@"XCOPY ""{file.SourceFilePath}"" ""{file.DestinationFolderPath}"" /I /Y");
+            }
+
+            sb.AppendLine($@"""C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"" ""{SlnName}""");
+
+            sb.AppendLine("cmd /k");
+
+            return sb.ToString();
+        }
+
+        private void ScriptPreviewCommandExecute(object parameter)
+        {
+            ScriptEdit = CreateScript();
+        }
+
+        private void OutputCommandExecute(object parameter)
+        {
+            File.WriteAllText("build.bat", ScriptEdit);
+
+            if (File.Exists("build.bat"))
+            {
+                System.Windows.MessageBox.Show("Success");
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Failed!");
+            }
+
+        }
         private void AddCopyPathCommandExecute(object parameter)
         {
             SelectFilePathView dialog = new SelectFilePathView();
@@ -48,8 +109,16 @@ namespace VisualStudioBuildScriptGenerator
             {
                 var sourceFilePath = (dialog.DataContext as IDialog).SourceFilePath;
                 var destinationPath = (dialog.DataContext as IDialog).SourceFilePath;
-                FilesCopyPath.Add($"{sourceFilePath}, {destinationPath}");
+                FilesCopyPath.Add(
+                    new FromToPathModel { SourceFilePath = sourceFilePath, DestinationFolderPath = destinationPath });
             }
         }
+    }
+
+    class FromToPathModel
+    {
+        public string SourceFilePath { get; set; }
+
+        public string DestinationFolderPath { get; set; }
     }
 }
