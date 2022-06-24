45d37f824732b0fc3471aeee00576647ce4f9827
BingFengHung <joe94008@gmail.com>
2022-06-24 14:04:21 +0800

    <feat>: 建立方案組態與方案平台的下拉式選單

--- src/VisualStudioBuildScriptGenerator/MainViewModel.cs
+++ src/VisualStudioBuildScriptGenerator/MainViewModel.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
 namespace VisualStudioBuildScriptGenerator
 {
-    class MainViewModel
+    class MainViewModel : ViewModelBase
     {
+        public MainViewModel()
+        {
+            Configurations = new ObservableCollection<OptionModel>
+            {
+                new OptionModel{Name = "Debug", Value = "Debug"},
+                new OptionModel{Name = "Release", Value = "Release"},
+            };
+
+            Platforms = new ObservableCollection<OptionModel>
+            {
+                new OptionModel{Name = "x86", Value = "x64"},
+                new OptionModel{Name = "x64", Value = "x64"},
+            };
+
+            ConfigurationSelected = Configurations[0];
+            PlatformSelected = Platforms[0];
+        }
+
+        public ObservableCollection<OptionModel> Configurations { get; set; }
+
+        public ObservableCollection<OptionModel> Platforms { get; set; }
+
+        public OptionModel ConfigurationSelected { get; set; }
+
+        public OptionModel PlatformSelected { get; set; }
     }
 }
